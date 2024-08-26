19075147c7d370b2b084a0ef30db987a72d58776
david <davidaliev664@gmail.com>
2024-08-26 09:37:57 +0300

    feat:(page): New User page

--- components/navbar/SignOut.tsx
+++ components/navbar/SignOut.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+"use client";
+
+import { SignOutButton } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
 
 function SignOut() {
-  return <div>SignOut</div>;
+  const { toast } = useToast();
+  const logoutHandle = () => {
+    toast({ description: "you have been sign out" });
+  };
+  return (
+    <SignOutButton redirectUrl="'">
+      <button className="w-full text-left" onClick={logoutHandle}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
 }
 
 export default SignOut;
