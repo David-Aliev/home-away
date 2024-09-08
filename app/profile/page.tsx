cc7dbe2f45dad2ac9d3f4260bebde89cc85ce685
David-Aliev <davidaliev664@gmail.com>
2024-09-08 14:58:01 +0300

    feat: safeparse method applied to update profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,42 @@
-function ProfilePage() {
-  return <div>ProfilePage</div>;
+import FormContainer from "@/components/form/FormContainer";
+import { updateProfileAction, fetchProfile } from "@/utils/actions";
+import FormInput from "@/components/form/FormInput";
+import { SubmitButton } from "@/components/form/Buttons";
+
+async function ProfilePage() {
+  const profile = await fetchProfile();
+
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">user profile</h1>
+      <div className="border p-8 rounded-md">
+        {/* image input container */}
+        <FormContainer action={updateProfileAction}>
+          <div className="grid md:grid-cols-2 gap-4 mt-4">
+            <FormInput
+              defaultValue={profile.firstName}
+              placeholder="David"
+              type="text"
+              name="firstName"
+            />
+            <FormInput
+              defaultValue={profile.lastName}
+              placeholder="Aliev"
+              type="text"
+              name="lastName"
+            />
+            <FormInput
+              defaultValue={profile.username}
+              type="text"
+              name="username"
+              label="Username"
+            />
+          </div>
+          <SubmitButton text="Update profile" className="mt-8" />
+        </FormContainer>
+      </div>
+    </section>
+  );
 }
 
 export default ProfilePage;
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,10 +1,27 @@
 "use server";
 
-import { profileScheme } from "./schemas";
+import { profileScheme, validateWithZodSchema } from "./schemas";
 import db from "./db";
 import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("you must be log in to acces this route");
+  }
+
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "an error occured",
+  };
+};
 
 export const createProfileAction = async (
   prevState: any,
@@ -14,7 +31,8 @@ export const createProfileAction = async (
     const user = await currentUser();
     if (!user) throw new Error("ples log in to create a profile");
     const rawData = Object.fromEntries(formData);
-    const validatedFields = profileScheme.parse(rawData);
+    const validatedFields = validateWithZodSchema(profileScheme, rawData);
+
     await db.profile.create({
       data: {
         clerkId: user.id,
@@ -29,9 +47,7 @@ export const createProfileAction = async (
       },
     });
   } catch (error) {
-    return {
-      message: error instanceof Error ? error.message : "an error occured",
-    };
+    return renderError(error);
   }
   redirect("/");
 };
@@ -51,4 +67,42 @@ export const fetchProfileImage = async () => {
   });
 
   return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+
+  if (!profile) redirect("/profile/create");
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(profileScheme, rawData);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields,
+    });
+
+    revalidatePath("/profile");
+    return { message: "Profile updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+
+  return { message: "update profile action" };
 };
